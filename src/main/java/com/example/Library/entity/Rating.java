306c5d5cfc5dbd1b26bec1ff873754ee819655d3
XxxAnduXaaA <nekit991tka2017@gmail.com>
2025-02-06 16:09:04 +0400

    refactor('rating','review') comment trash classes

--- src/main/java/com/example/Library/entity/Rating.java
+++ src/main/java/com/example/Library/entity/Rating.java
@@ -1,27 +1,27 @@
-package com.example.Library.entity;//package com.example.library.entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Table(name = "ratings")
-@Data
-public class Rating {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false)
-    private int score;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "book_id", nullable = false)
-    private Book book;
-
-}
-
+//package com.example.Library.entity;//package com.example.library.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Entity
+//@Table(name = "ratings")
+//@Data
+//public class Rating {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private int score;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "book_id", nullable = false)
+//    private Book book;
+//
+//}
+//
--- src/main/java/com/example/Library/entity/Review.java
+++ src/main/java/com/example/Library/entity/Review.java
@@ -1,30 +1,30 @@
-package com.example.Library.entity;//package com.example.library.entity;
-
-import jakarta.persistence.*;
-import lombok.Data;
-
-@Entity
-@Table(name = "reviews")
-@Data
-public class Review {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(columnDefinition = "TEXT", nullable = false)
-    private String text;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @ManyToOne
-    @JoinColumn(name = "book_id", nullable = false)
-    private Book book;
-
-    @Column(nullable = false)
-    private String date;
-
-}
-
+//package com.example.Library.entity;//package com.example.library.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//@Entity
+//@Table(name = "reviews")
+//@Data
+//public class Review {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(columnDefinition = "TEXT", nullable = false)
+//    private String text;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @ManyToOne
+//    @JoinColumn(name = "book_id", nullable = false)
+//    private Book book;
+//
+//    @Column(nullable = false)
+//    private String date;
+//
+//}
+//
