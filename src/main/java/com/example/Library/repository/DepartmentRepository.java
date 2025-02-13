c49298b93cc54ae2bb89b82632355192c5d7e338
XxxAnduXaaA <nekit991tka2017@gmail.com>
2025-02-13 21:26:54 +0400

    feature(DepartmentRepository): add DepartmentRepository to make interaction between database and entity

---
+++ src/main/java/com/example/Library/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.example.Library.repository;
+
+import com.example.Library.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department findByDepartmentName(String name);
+}
